004fe53d97fd6fadb5bf93ec9cecbbea0851b4b4
MGX DEV <mgx.coding@gmail.com>
2023-12-27 16:39:47 +0100

    Showcase : added nuidity

--- projects/showcase/src/app/app.config.ts
+++ projects/showcase/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideNuidityConfig } from '../nuidity/nuidity.config.module';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), provideNuidityConfig()],
 };
---
+++ projects/showcase/src/nuidity/nuidity.config.module.ts
@@ -0,0 +1,24 @@
+import {
+  EnvironmentProviders,
+  NgModule,
+  Provider,
+  makeEnvironmentProviders,
+} from '@angular/core';
+
+/**
+ * This file serves as the configuration file for the n-ui-dity library.
+ * You can provide default configurations for all of the configurable features.
+ * Refer to the documentation, or use intellisense, to know how to fill your configurations.
+ */
+
+// Put all of your configurations into this array
+const providers: (Provider | EnvironmentProviders)[] = [];
+
+// If you do not use a standalone application, import this module into your application module
+@NgModule({ providers })
+export class NuidityConfigModule {}
+
+// If you use a standalone application, call this function inside your app configuration file
+export function provideNuidityConfig(): EnvironmentProviders {
+  return makeEnvironmentProviders(providers);
+}
